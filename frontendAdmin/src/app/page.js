03639d01313208c140bb0d173ab1e67b9d4cf773
Khanna <kanish.khanna@ubc.ca>
2025-04-08 22:46:37 -0700

    fix: changed to cognito

--- frontendAdmin/src/app/page.js
+++ frontendAdmin/src/app/page.js
@@ -5,7 +5,7 @@
   import { Amplify } from "aws-amplify";
 
   Amplify.configure({
-    Auth: {
+    Cognito: {
       region: process.env.NEXT_PUBLIC_AWS_REGION,
       userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
       userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID,
